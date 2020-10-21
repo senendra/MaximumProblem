496685ef40c5033128b34278a55a4e7d13af4b60
Senendra <senendra.007@gmail.com>
2020-10-22 00:01:46 +0530

    [Senendra] Add: Adding method to get maximum of 3 integers

---
+++ MaximumProblem/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MaximumProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Maximum Problem Program");
+            int firstNum = 3, secondNum = 2, thirdNum = 10;
+            Console.WriteLine("Maximum of " + firstNum + " , " + secondNum + " , " + thirdNum + " is : " + FindMaximum.MaxValue(firstNum, secondNum, thirdNum));
+        }
+    }
+}
