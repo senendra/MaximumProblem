962a3c180539aa8ec4b1c239a676453b786dc850
Senendra <senendra.007@gmail.com>
2020-10-22 00:16:16 +0530

    [Senendra] Add: Adding Unit Test Method Testcase 1.2 for getting maximum at second position out of 3 integers

--- UnitTestMaxProblem/UnitTest1.cs
+++ UnitTestMaxProblem/UnitTest1.cs
@@ -17,5 +17,16 @@ namespace UnitTestMaxProblem
             //Assert
             Assert.AreEqual(expected, result);
         }
+        [TestMethod]
+        public void GetSecondPositionIntegerMaximumOutOfThree()
+        {
+            //Arrange 
+            int firstNum = 10, secondNum = 15, thirdNum = 3;
+            int expected = secondNum;
+            //Act 
+            int result = FindMaximum.MaxValue(firstNum, secondNum, thirdNum);
+            //Assert
+            Assert.AreEqual(expected, result);
+        }
     }
 }
